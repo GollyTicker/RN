686e5e0beee6d08b2916fbd2239301e8c8af2535
GollyTicker <golly.ticker@gmail.com>
2014-04-03 13:17:21 +0200

    server almost fin

--- src/Server.java
+++ src/Server.java
@@ -1,13 +1,17 @@
 /**
  * Created by Allquantor on 02.04.14.
  */
-import com.javafx.tools.doclets.formats.html.SourceToHTMLConverter;
-
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
+    private final String CONNECTION_CLOSE = "BYE";
+    private final String LOWERCASE = "LOWERCASE";
+    private final String UPPERRCASE = "UPPERCASE";
+    private final String REVERSE = "REVERSE";
+    private final String SHUTDOWN = "SHUTDOWN";
+
     public static void  main(String[] args){
 
         try {
@@ -17,35 +21,78 @@ public class Server {
 
             //loop
             while(true){
-                System.out.println("0");
+                //System.out.println("0");
 
                 Socket clientSocket = welcomeSocket.accept();
                 InputStream is = clientSocket.getInputStream();
 
-                System.out.println("1");
+                //System.out.println("1");
                 BufferedReader in = new BufferedReader(
                         new InputStreamReader(is));
 
-                System.out.println("2");
+                //System.out.println("2");
 
                 PrintWriter out =
                         new PrintWriter(clientSocket.getOutputStream(), true);
 
-                System.out.println("3");
+                //System.out.println("3");
 
                 String line = in.readLine();
                 while(line != null) {
-                    System.out.println("DAS IST INPUT Line" + sTest);
-                    out.println(sTest);
+                    System.out.println("Gelesen: " + line);
+
+                    String resp = respondToCommand(line);
+
+                    out.println(resp);
                     out.flush();
+
+                    if (resp.equals(CONNECTION_CLOSE)) {
+                        clientSocket.close();
+                        break;
+                    }
+                    line = in.readLine();
                 }
-                clientSocket.close();
             }
 
         } catch (IOException e) {
             e.printStackTrace();
         }
 
+    }
+
+    String respondToCommand(String input) {
+        if (input.startsWith(CONNECTION_CLOSE)) {
+            return CONNECTION_CLOSE;
+        }
+        if (input.startsWith(LOWERCASE)) {
+            return input.getParam().toUpperCase();
+        }
+        else if (input.startsWith(UPPERCASE)) {
+            return input.getParam().toLowerCase();
+        }
+        else if (input.startsWith(REVERSE)) {
+            String str = inout.getParam();
+            return new StringBuffer(str).reverse().toString();
+        }
+        else if (input.startsWith(SHUTDOWN)) {
+            String pw = getParam(input);
+            // what to do here?
+            return "yeah... shutdown?";
+        }
+        else {
+            return unknownCommand(input);
+        }
+    }
+
+    // zu einem Kommando der Form "CMD <param>" liefert diese Methode das Param zurueck.
+    String getParam(String str) {
+        int seperator = str.indexOf(' ');
+        if (seperator == -1) {
+            return str;
+        }
+        else {
+            return str.substring(seperator + 1);
+        }
     }
 
 }
